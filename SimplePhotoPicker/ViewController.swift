55556cbfdddcd8c927931d9bbe489a8f487fbb9e
Asif Bilal <asifbilal786@gmail.com>
2016-06-03 21:33:11 +0500

    Initial Commit

---
+++ SimplePhotoPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimplePhotoPicker
+//
+//  Created by Asif Bilal on 03/06/2016.
+//  Copyright © 2016 Asif Bilal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
